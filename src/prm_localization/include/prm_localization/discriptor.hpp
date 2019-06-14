bb7b3f703e86c94fe52b442739ff7d97f0f81404
Yang <lzyplayer@stu.xjtu.edu.cn>
2019-06-14 22:07:19 +0800

    working on desp

--- src/prm_localization/include/prm_localization/discriptor.hpp
+++ src/prm_localization/include/prm_localization/discriptor.hpp
@@ -19,56 +19,92 @@ using pointT=pcl::PointXYZ;
 namespace prm_localization {
 
     class discriptor {
+    //descriptors max num
+    static const int MAX_DES = 9000;
+
     public:
         /**
          * extractEig feature from cloud with spec gridstep
          * @param src_cloud
          * @param gridstep
          */
-        void extractEig(const pcl::PointCloud<pointT>::Ptr src_cloud, float gridstep){
+        discriptor(const pcl::PointCloud<pointT>::Ptr src_cloud, float gridstep) : gridstep(gridstep) {
             //down sample
             boost::shared_ptr<pcl::VoxelGrid<pointT>> voxelgrid (new pcl::VoxelGrid<pointT>());
             voxelgrid->setLeafSize(gridstep,gridstep,gridstep);
             voxelgrid->setInputCloud(src_cloud);
             pcl::PointCloud<pointT>::Ptr down_src_cloud (new pcl::PointCloud<pointT>());
             voxelgrid->filter(*down_src_cloud);
-            //ready gridstep
+            //ready grid step
             boost::shared_array< float > gridsteps (new float_t[4]);
             for (int i = 0; i < 4 ; ++i) {
                 gridsteps[i] = (i+1)*2*gridstep;
             }
-
-
-
+            //select descriptor points
+            select_desp_index.resize(MAX_DES/20);
+            select_desp_index.clear();
+            std::vector<std::vector<int>>  srcIdx;
+            srcIdx.resize(MAX_DES/20);
+            srcIdx.clear();
+            range_search(src_cloud,down_src_cloud,select_desp_index,srcIdx,gridsteps[0]);
+            //
 
         }
 
-        const boost::shared_ptr<Matrix<double, 9, -1, 0, 9, 9000>> &getDespcript() const {
+        void range_search(const pcl::PointCloud<pointT>::Ptr src_cloud,const pcl::PointCloud<pointT>::Ptr down_cloud,
+                std::vector<uint32_t > &srcSeedIndex,std::vector<std::vector<int>> &srcIdx,float radius){
+            pcl::KdTreeFLANN< pointT > kdTreeFlann;
+            kdTreeFlann.setInputCloud(src_cloud);
+            std::vector<int> srcIdxPer;
+            std::vector<float> pointRadiusSquaredDistance;
+            for (int i=0;i<down_cloud->size();++i){
+                if(kdTreeFlann.radiusSearch(down_cloud->points[i],radius,srcIdxPer,pointRadiusSquaredDistance)>0){
+                    if (srcIdxPer.size()>=10) {
+                        srcSeedIndex.push_back(i);
+                        srcIdx.push_back(srcIdxPer);
+                    }
+                }
+            }
+            psize = srcSeedIndex.size();
+        }
+
+
+
+        
+    private:
+
+        boost::shared_ptr<Matrix<double ,9,-1,0,9,MAX_DES>> despcrip;
+        boost::shared_ptr<Matrix<double ,3,-1,0,3,MAX_DES>> position;
+        boost::shared_ptr<Matrix<double ,12,-1,0,12,MAX_DES>> norm;
+        std::vector< uint32_t >select_desp_index;
+        float gridstep;
+        double psize;
+
+
+    public:
+        const boost::shared_ptr<Matrix<double, 9, -1, 0, 9, MAX_DES>> &getDespcrip() const {
             return despcrip;
         }
 
-        const boost::shared_ptr<Matrix<double, 3, -1, 0, 3, 9000>> &getPosition() const {
+        const boost::shared_ptr<Matrix<double, 3, -1, 0, 3, MAX_DES>> &getPosition() const {
             return position;
         }
 
-        const boost::shared_ptr<Matrix<double, 12, -1, 0, 12, 9000>> &getNorm() const {
+        const boost::shared_ptr<Matrix<double, 12, -1, 0, 12, MAX_DES>> &getNorm() const {
             return norm;
+        }
+
+        const std::vector<uint32_t> &getSelectDespIndex() const {
+            return select_desp_index;
+        }
+
+        float getGridstep() const {
+            return gridstep;
         }
 
         double getPsize() const {
             return psize;
         }
-
-    private:
-        boost::shared_ptr<Matrix<double ,9,-1,0,9,9000>> despcrip;
-        boost::shared_ptr<Matrix<double ,3,-1,0,3,9000>> position;
-        boost::shared_ptr<Matrix<double ,12,-1,0,12,9000>> norm;
-        double psize;
-
-
-
-
-
 
 
     };
--- src/prm_localization/src/prm_localization.cpp
+++ src/prm_localization/src/prm_localization.cpp
@@ -1,6 +1,9 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <prm_localization/fdcp_register.hpp>
+//user test
+#include <vector>
+#include <iostream>
 
 using namespace std;
 class PRM_localization{
@@ -22,15 +25,24 @@ private:
 
 
 
-
+//
 int main(int argc, char *argv[])
 {
-
-    ros::init(argc, argv, "register");
-
-    PRM_localization prm_localization;
-
-    ros::spin();
-
+    vector<int>array(9);
+    array.push_back(2);
+    array.push_back(2);
+    array.push_back(2);
+    array.push_back(2);
+    array.clear();
+    cout<<array.size();
+    array.push_back(5);
+    array.push_back(3);
+    cout<<array.size();
+//    ros::init(argc, argv, "register");
+//
+//    PRM_localization prm_localization;
+//
+//    ros::spin();
+//
     return 0;
 }
