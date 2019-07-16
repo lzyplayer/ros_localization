ef68bce28fbedf73db2ad5a5978500070b414bae
Yang <lzyplayer@stu.xjtu.edu.cn>
2019-07-16 11:49:58 +0800

    structure_code_ICP

--- src/prm_localization/src/rt_localization_nodelet.cpp
+++ src/prm_localization/src/rt_localization_nodelet.cpp
@@ -6,7 +6,6 @@
 // ros_msg
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Pose2D.h>
-
 #include <nav_msgs/Odometry.h>
 // pcl
 #include <pcl/filters/voxel_grid.h>
@@ -15,7 +14,9 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/registration/icp.h>
+#include <pcl/common/transforms.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pclomp/ndt_omp.h>
 //#include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/pcd_io.h>
 //eigen
@@ -58,29 +59,38 @@ namespace rt_localization_ns{
             trim_high = private_nh.param<float>("trim_high", 4.0f);
             auto init_x = private_nh.param<float>("init_x", 0.0f);
             auto init_y = private_nh.param<float>("init_y", 0.0f);
-            auto init_raw = private_nh.param<double>("init_raw", 0.0);
+            auto init_yaw = private_nh.param<double>("init_yaw", 0.0);
             auto icpMaximumIterations = private_nh.param<int>("icpMaximumIterations", 50);
             auto icpRANSACOutlierRejectionThreshold = private_nh.param<float>("icpRANSACOutlierRejectionThreshold", 0.04f);
             auto icpTransformationEpsilon = private_nh.param<float>("icpTransformationEpsilon", 1e-8);
             auto downSampleSize = private_nh.param<float>("downSampleSize", 0.05f);
+            map_tf = private_nh.param<std::string>("map_tf", "map");
+            base_lidar_tf = private_nh.param<std::string>("base_lidar_tf", "velodyne");
+
+
             //init_pose
             curr_pose.setIdentity(4,4);
-            curr_pose.block(0,0,3,3) = euler2rot(0,0,init_raw);
+            curr_pose.block(0,0,3,3) = euler2rot(0,0,init_yaw);
             curr_pose(0,3) =init_x;
             curr_pose(1,3) =init_y;
             curr_pose_stamp =ros::Time(0.001);
-            //
-            icp.setMaximumIterations(icpMaximumIterations);
-            icp.setRANSACOutlierRejectionThreshold(icpRANSACOutlierRejectionThreshold);
-            icp.setMaxCorrespondenceDistance(icpRANSACOutlierRejectionThreshold*100);
-            icp.setTransformationEpsilon(icpTransformationEpsilon);
-//        icp.set
+            //ndt_omp
+//            pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>::Ptr ndt(new pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>());
+//            ndt->setTransformationEpsilon(0.01);
+//            ndt->setResolution(1.0);
+//            ndt->setNeighborhoodSearchMethod(pclomp::DIRECT7);
+//            registration = ndt;
+            //registration-icp
+            registration.setMaximumIterations(icpMaximumIterations);
+            registration.setRANSACOutlierRejectionThreshold(icpRANSACOutlierRejectionThreshold);
+            registration.setMaxCorrespondenceDistance(icpRANSACOutlierRejectionThreshold*100);
+            registration.setTransformationEpsilon(icpTransformationEpsilon);
+//        registration.set
             /**sub and pub**/
-//        odom_pub = nh.advertise<nav_msgs::Odometry>("velodyne_link",50);
+//            odom_pub = nh.advertise<nav_msgs::Odometry>(map_tf,50);
+            odom_pub = nh.advertise<nav_msgs::Odometry>("/odom",50);
+            get_pmsg_pub = nh.advertise<nav_msgs::Odometry>("/stamp",5);
             points_suber = nh.subscribe("/velodyne_points",1,&RealTime_Localization::points_callback,this);
-//            localmap_suber =  nh.subscribe("/localmap",1,&RealTime_Localization::localmap_callback,this);
-//            curr_pointcloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/registered_pointCloud",5);
-//            points_suber = mt_nh.subscribe("/velodyne_points",1,&RealTime_Localization::points_callback,this);
             localmap_suber =  mt_nh.subscribe("/localmap",1,&RealTime_Localization::localmap_callback,this);
             curr_pointcloud_pub = mt_nh.advertise<sensor_msgs::PointCloud2>("/registered_pointCloud",5);
             /**utility param**/
@@ -92,6 +102,11 @@ namespace rt_localization_ns{
     private:
 
         void points_callback(const sensor_msgs::PointCloud2ConstPtr& points_msg){
+            //send get flag (optional)
+            nav_msgs::Odometry flag_odom;
+            flag_odom.header.stamp=points_msg->header.stamp;
+            get_pmsg_pub.publish(flag_odom);
+            // convert ros msg
             pcl::PointCloud<pcl::PointXYZ>::Ptr curr_cloud (new pcl::PointCloud<pcl::PointXYZ>());
             pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud (new pcl::PointCloud<pcl::PointXYZ>());
             pcl::fromROSMsg(*points_msg, *curr_cloud);
@@ -100,26 +115,12 @@ namespace rt_localization_ns{
             downSampler.filter(*filtered_cloud);
             //trim and 2d
             auto flat_cloud  = trimInputCloud(filtered_cloud, trim_low, trim_high);
-
-            //icp
-            icp.setInputTarget(localmap_cloud);
-            icp.setInputSource(flat_cloud);
-            pcl::PointCloud<pcl::PointXYZ> result_cloud ;
-            //icp start
-            clock_t start = clock();
-            icp.align(result_cloud,curr_pose);//,curr_pose
-            clock_t end = clock();
-            NODELET_INFO("icp regis time = %f seconds",(double)(end  - start) / CLOCKS_PER_SEC);
-//            NODELET_INFO("fitness score = %f ",icp.getFitnessScore());
-//            NODELET_INFO(" ");
-            Matrix4f transform = icp.getFinalTransformation();
-//        Vector3f euler = rot2euler(transform.block(0,0,3,3));
-//        NODELET_INFO("x = %f, y = %f, theta = %f ",transform(0,3),transform(1,3),euler(2));
-            //
+            //pc register
+            Matrix4f transform = pc_register(flat_cloud,localmap_cloud,curr_pose);
             {
                 lock_guard<mutex> lockGuard(curr_pose_mutex);
                 if(points_msg->header.stamp>curr_pose_stamp){
-                    curr_pose=icp.getFinalTransformation();
+                    curr_pose=transform;
                     NODELET_INFO("time to former stamp = %f seconds",points_msg->header.stamp.toSec()-curr_pose_stamp.toSec());
                     curr_pose_stamp = points_msg->header.stamp;
 
@@ -130,24 +131,39 @@ namespace rt_localization_ns{
             }
 
             //publish tf
-            transformBroadcaster.sendTransform(matrix2transform(points_msg->header.stamp,curr_pose,"map","velodyne"));
-
+            transformBroadcaster.sendTransform(matrix2transform(points_msg->header.stamp,curr_pose,map_tf,base_lidar_tf));
+            //publish odom (optional)
+            nav_msgs::Odometry odom;
+            odom.header.stamp=points_msg->header.stamp;
+            odom.header.frame_id=map_tf;
+            odom.child_frame_id=base_lidar_tf;
+            odom.pose.pose.position.x=transform(0,3);
+            odom.pose.pose.position.y=transform(1,3);
+            odom.pose.pose.position.z=transform(2,3);
+            Eigen::Quaternionf q = rot2quat(transform);
+            odom.pose.pose.orientation.x=q.x();
+            odom.pose.pose.orientation.y=q.y();
+            odom.pose.pose.orientation.z=q.z();
+            odom.pose.pose.orientation.w=q.w();
+            odom_pub.publish(odom);
 
             //publish cloud (optional)
-            pcl::PointCloud<pcl::PointXYZRGB>::Ptr coloredCloud (new pcl::PointCloud<pcl::PointXYZRGB>());
-            coloredCloud->width = result_cloud.width;
-            coloredCloud->height = 1;//result_cloud.height
-            coloredCloud->points.resize(coloredCloud->width*coloredCloud->height);
-
-            for (size_t i=0;i<result_cloud.width;i++){
-                coloredCloud->points[i].x = result_cloud.points[i].x;
-                coloredCloud->points[i].y = result_cloud.points[i].y;
-                coloredCloud->points[i].z = result_cloud.points[i].z;
-                coloredCloud->points[i].r=255;
-                coloredCloud->points[i].g=255;
+            pcl::PointCloud<pcl::PointXYZ>::Ptr transformedCloud (new pcl::PointCloud<pcl::PointXYZ>());
+            pcl::PointCloud<pcl::PointXYZRGBA>::Ptr coloredCloud (new pcl::PointCloud<pcl::PointXYZRGBA>());
+            pcl::PointCloud<pcl::RGB>::Ptr color (new pcl::PointCloud<pcl::RGB>());
+            pcl::transformPointCloud(*curr_cloud,*transformedCloud,transform);
+            color->width = transformedCloud->width;
+            color->height = 1;//result_cloud.height
+            color->points.resize(color->width*color->height);
+            for (size_t i=0;i<transformedCloud->width;i++){
+                color->points[i].r=255;
+                color->points[i].g=255;
+                color->points[i].a=100;
             }
+            pcl::concatenateFields(*transformedCloud,*color,*coloredCloud);
+
             pcl_conversions::toPCL(points_msg->header,coloredCloud->header);
-            coloredCloud->header.frame_id = "map";
+            coloredCloud->header.frame_id = map_tf;
             curr_pointcloud_pub.publish(coloredCloud);
 
 
@@ -187,8 +203,21 @@ namespace rt_localization_ns{
             return cloudTrimmed;
         }
 
-        Eigen::Matrix3d pc_register(const pcl::PointCloud<pcl::PointXY>::ConstPtr& curr_cloud,const pcl::PointCloud<pcl::PointXY>::ConstPtr& local_map,const Eigen::Matrix3d& initial_matrix){
-
+        Eigen::Matrix4f pc_register(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& curr_cloud,const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& local_map,const Eigen::Matrix4f& initial_matrix){
+            //registration
+            registration.setInputSource(curr_cloud);
+            registration.setInputTarget(local_map);
+            pcl::PointCloud<pcl::PointXYZ> result_cloud ;
+            //registration start
+            clock_t start = clock();
+            registration.align(result_cloud,curr_pose);//,curr_pose
+            clock_t end = clock();
+            NODELET_INFO("registration regis time = %f seconds",(double)(end  - start) / CLOCKS_PER_SEC);
+//            NODELET_INFO("fitness score = %f ",registration.getFitnessScore());
+//            NODELET_INFO(" ");
+            return registration.getFinalTransformation();
+//        Vector3f euler = rot2euler(transform.block(0,0,3,3));
+//        NODELET_INFO("x = %f, y = %f, theta = %f ",transform(0,3),transform(1,3),euler(2));
         }
 
     private:
@@ -207,14 +236,20 @@ namespace rt_localization_ns{
         ros::Subscriber odom_suber;
         ros::Subscriber points_suber;
         ros::Subscriber localmap_suber;
-//    ros::Publisher odom_pub;
+        ros::Publisher odom_pub;
+        ros::Publisher get_pmsg_pub;
         ros::Timer timer;
         tf::TransformBroadcaster transformBroadcaster;
+        //tf
+        string map_tf;
+        string base_lidar_tf;
+
         // clouds
         pcl::PointCloud<pcl::PointXYZ>::Ptr localmap_cloud;
 
         // utility
-        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+//        pcl::Registration<pcl::PointXYZ, pcl::PointXYZ>::Ptr registration;
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> registration;
         pcl::VoxelGrid<pcl::PointXYZ> downSampler;
         std::mutex curr_pose_mutex;
         // time log
