017efb46f6c44e46fece0a43b1ad215a07b1eb0e
Midou <qing1159364090@stu.xjtu.edu.cn>
2019-08-15 11:26:31 +0800

    update

--- src/ukf/include/ukf_core/models/smodel_ukf2d.hpp
+++ src/ukf/include/ukf_core/models/smodel_ukf2d.hpp
@@ -17,18 +17,25 @@ struct SModelParameter : KalmanFilterModelParameterBase
     SModelParameter() //构造函数
     {
         this->process_noise = Eigen::MatrixXd::Identity(11, 11);
-        this->process_noise.middleRows(0, 2) *= 0.1;//pos
-        this->process_noise.middleRows(2, 2) *= 0.1;//v
-        this->process_noise.middleRows(4, 1) *= 0.005;//yaw
+        this->process_noise.middleRows(0, 2) *= 0.05;//pos
+        this->process_noise.middleRows(2, 2) *= 0.05;//v
+        this->process_noise.middleRows(4, 1) *= 0.05;//yaw
         this->process_noise.middleRows(5, 2) *= 1;//a
         this->process_noise.middleRows(7, 1) *= 0.01;//w
         this->process_noise.middleRows(8, 2) *= 1e-6;//ab
         this->process_noise.middleRows(10, 1) *= 1e-6;//wb
     }
      //重载
-    SModelParameter(MatrixXd process_noise_in)
+    SModelParameter(double pos, double v, double yaw, double a, double w)
     {
-        this->process_noise = process_noise_in;
+        this->process_noise = Eigen::MatrixXd::Identity(11, 11);
+        this->process_noise.middleRows(0, 2) *= pos;//pos
+        this->process_noise.middleRows(2, 2) *= v;//v
+        this->process_noise.middleRows(4, 1) *= yaw;//yaw
+        this->process_noise.middleRows(5, 2) *= a;//a
+        this->process_noise.middleRows(7, 1) *= w;//w
+        this->process_noise.middleRows(8, 2) *= 1e-6;//ab
+        this->process_noise.middleRows(10, 1) *= 1e-6;//wb
     }
 
     Eigen::MatrixXd process_noise;
@@ -51,6 +58,7 @@ public:
     )
     {
         xsig.setZero(11, 1);
+
         xsig_covariance.setIdentity(11,11);
         xsig_covariance *= 0.1;
     }
@@ -63,12 +71,10 @@ public:
         FilterVector& xsig_prediction
     )//属于类的函数
     {
-        // std::cout << "fine1" << std::endl;
         Vector2d p = xsig.middleRows(0, 2);
         Vector2d v = xsig.middleRows(2, 2);
         double yaw = xsig(4);
         Vector2d raw_acc = xsig.middleRows(5, 2);//状态，对应的是IMU系下的加速度
-        //std::cout << "raw_acc:" << raw_acc << std::endl; 
         double raw_w = xsig(7);
 
         Vector2d acc_bias = xsig.middleRows(8, 2);
@@ -78,7 +84,9 @@ public:
 
         double ax = acc_(0);
         double ay = acc_(1);
-        // double ax = 0;
+        ay = 0;
+        acc_(1) = 0;
+        ax = 0;
         // double ay = 0;
         double vx = v(0);
         double vy = v(1);
@@ -88,7 +96,6 @@ public:
         double delta2 = delta * delta;
         double yawt = yaw + w * delta;
         //pos
-        cout << "w" << w << endl;
         if(abs(w) > LIMITW)
         {
             a1 = vy*(cos(yawt)-cos(yaw)) + vx*(sin(yawt)-sin(yaw));
@@ -104,7 +111,7 @@ public:
             xsig_prediction(1) = p(1) + a2 + (1/2.0) * delta2 * (ay*cos(yaw) + ax*sin(yaw));
         }
         //v yaw
-        xsig_prediction.middleRows(2, 2) = v;// + acc_ * delta;
+        xsig_prediction.middleRows(2, 2) = v;// +  acc_ * delta;
         xsig_prediction(4) = yawt;
 
         xsig_prediction.middleRows(5, 2) = xsig.middleRows(5, 2);		// constant acceleration
@@ -114,6 +121,5 @@ public:
     }
 public:
     int num;
-    
 };
 #endif
